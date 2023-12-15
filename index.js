5d3e26435e80dbb2c5164180a888423c6893b364
tuananh0303 <anhhello564@gmail.com>
2023-12-16 04:00:14 +0700

    updated

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const Device = require("./models/DeviceModel");
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = socketIO(server);
 
 const PORT = process.env.PORT || 8080;
 const URI = process.env.mongo_URL;
 
-app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 app.use(bodyParser.json());
